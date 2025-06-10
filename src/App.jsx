0e8f852f6ee862d88a1530c6dbdb63bb81e3f0f2
David <davidmillett@protonmail.com>
2025-06-10 15:19:42 +0100

    created table of pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
+import pokemon_data from "./utils/data"
+
 const App = () => {
 
+  const columns = ['number', 'name', 'types']
+
+  const pokemon = pokemon_data
+
+  
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Pokedex</h1>
+      <table>
+        <thead>
+          <tr>
+            {columns.map((column) => {
+              return (
+                <th key={column}>
+                  {column.charAt(0).toUpperCase() + column.slice(1)}
+                </th>
+              )
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {pokemon.map((mon) => {
+            return (
+              <tr key={mon.number}>
+                {columns.map((column) => {
+                  return (
+                    <td key={column}>{mon[column]}</td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      {/* {pokemon.map((mon) => {
+        return (
+          <p>{mon.name}</p>
+        )
+      })} */}
+    </>
   )
 }
 
