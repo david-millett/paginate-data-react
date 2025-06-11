3237bc00ea5219db0c4bd535396c1e991bf6e894
David <davidmillett@protonmail.com>
2025-06-11 16:56:59 +0100

    reorganised and tidied code

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import pokemon_data from "./utils/data"
 
 const App = () => {
 
+  // Variables
   const columns = ['number', 'name', 'types']
   const pokemon = pokemon_data
   
   const [page, setPage] = useState(0)
   const [pageLength, setPageLength] = useState(10)
+
   const pageLast = Math.ceil(pokemon.length / pageLength)
 
+  // Function to generate an array of all pages
   const getPageNumbers = (pageFirst, pageLast) => {
-    return Array.from({ length: pageLast - pageFirst + 1}, (_, i) => pageFirst + i)
+    return Array.from({ length: pageLast - pageFirst}, (_, i) => pageFirst + i)
   }
-  const pages = getPageNumbers(0, pageLast - 1)
-  console.log(pages)
+  const pages = getPageNumbers(0, pageLast)
 
+  // Function to isolate the data required for the current page
   const getPage = (page, pageLength) => {
     const itemFirst = page * pageLength
     const itemLast = (page + 1) * pageLength
@@ -25,9 +28,9 @@ const App = () => {
       last: itemLast
     }
   }
-
   const currentPage = getPage(page, pageLength)
 
+  // Function to change number of items per page and reset page to the beginning
   const changePageLength = (e) => {
     setPageLength(e.target.value)
     setPage(0)
@@ -72,16 +75,6 @@ const App = () => {
       <p>{`Showing ${currentPage.first + 1}-${Math.min(currentPage.last, pokemon.length)} of ${pokemon.length}`}</p>
       
       <div className="flex">
-        <select id="pageLength" name="pageLength" value={pageLength} onChange={changePageLength}>
-          <option value={5}>5</option>
-          <option value={10}>10</option>
-          <option value={15}>15</option>
-          <option value={20}>20</option>
-        </select>
-        <p>pokemon per page</p>
-      </div>
-      
-      <div className="flex">
         <p>Page</p>
         <select id="page" name="page" value={page + 1} onChange={(e) => {setPage(e.target.value - 1)}}>
           {pages.map((pg) => {
@@ -89,6 +82,16 @@ const App = () => {
           })}
         </select>
         <p>{`of ${pageLast}`}</p>
+      </div>
+      
+      <div className="flex">
+        <select id="pageLength" name="pageLength" value={pageLength} onChange={changePageLength}>
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+          <option value={15}>15</option>
+          <option value={20}>20</option>
+        </select>
+        <p>pokemon per page</p>
       </div>
     </>
   )
